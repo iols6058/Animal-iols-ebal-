ef6bc3aa9b629702c17d7ce2bf53814153365fbd
ebal8006 <31479766+ebal8006@users.noreply.github.com>
2018-02-08 08:41:26 -0700

    retpil
    reptile

--- src/Reptile.java
+++ src/Reptile.java
@@ -1,6 +1,35 @@
 /**
  * Created by ebal8006 on 1/31/2018.
  */
-public class Reptile extends Animal
-{
+class Reptile extends Animal {
+    public Reptile(String species, boolean living, boolean awake, boolean hungry){
+        super(species, living, awake, hungry);
+    }
+    @Override
+    public String breathe(){
+        return "Lungs";
+    }
+    public String eat(){
+        return "Mainly other animals";
+    }
+    public String poop(){
+        return "Yeah";
+    }
+    public String move(){
+        return "Crawls";
+    }
+    public void lizard(){
+        Reptile lizard = new Reptile();
+        lizard.setSpecies("Lizard");
+        lizard.setVertebrate(true);
+        lizard.setFurred(false);
+        lizard.setScales(true);
+        lizard.setEggs(true);
+        lizard.setMilk(false);
+        lizard.setWarmblooded(true);
+        lizard.breathe();
+        lizard.eat();
+        lizard.poop();
+        lizard.move();
+    }
 }
